f3d8a9b4f4d87f70d06d27c3aee29b3d9850c76f
s1f10210223 <s1f102102234@iniad.org>
2021-11-05 22:57:03 +0900

    Create pattern2.js

---
+++ pattern2.js
@@ -0,0 +1,7 @@
+function sample2_4_1() {
+    let textarea = document.getElementById('textarea');
+    let content = document.getElementById('content');
+    let value = textarea.value;
+    content.innerHTML = value;
+  }
+    
